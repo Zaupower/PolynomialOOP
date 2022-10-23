8d62dc7585e91f81af4efb89b180d00090008d76
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-23 15:03:37 +0100

    Create polynomial class

---
+++ PolynomialOOP/PolynomialOOP/Polynomial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PolynomialOOP
+{
+    public class Polynomial
+    {
+        Polynomial()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Person: ";
+        }
+    }
+}
