121bb49731802004c011638098fb8afb4d5e4856
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-23 15:36:00 +0100

    overload methods and calculate logic

--- PolynomialOOP/PolynomialOOP/Polynomial.cs
+++ PolynomialOOP/PolynomialOOP/Polynomial.cs
@@ -5,14 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+
+//https://www.geeksforgeeks.org/program-add-two-polynomials/
 namespace PolynomialOOP
 {
     public class Polynomial
     {
-        Polynomial()
+        string[] coefficients;
+        Polynomial(string[] coefficients)
         {
-
+            this.coefficients = new string[coefficients.Length];
+            this.coefficients = coefficients;
         }
+
+        public static Polynomial operator +(Polynomial a,Polynomial b)
+        {
+            string[] l = new string[] {"a", "b"};
+            return new Polynomial(l);
+        }
+
+
+        public static Polynomial operator -(Polynomial a, Polynomial b)
+        {
+            string[] l = new string[] { "a", "b" };
+            return new Polynomial(l);
+        }
+        public static Polynomial operator *(Polynomial a, Polynomial b)
+        {
+            string[] l = new string[] { "a", "b" };
+            return new Polynomial(l);
+        }
+
 
         public override string ToString()
         {
